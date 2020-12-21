640dc58360e95a97c704f585da0e4321f562aff2
frank salvo <fhsalvo@yahoo.com>
2020-12-21 14:56:21 -0500

    bringing back 250 from api call

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default {
   getEmployees: function(query) {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us&inc=name,email,phone,id,picture,dob")
+    return axios.get("https://randomuser.me/api/?results=250&nat=us&inc=name,email,phone,id,picture,dob")
      ;
   }
 };
